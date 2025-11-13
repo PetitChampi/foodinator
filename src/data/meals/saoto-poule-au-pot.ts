1ebd2c498a6cb8eaacab653356be53c405c7c075
Esther Haddon <exablob75@gmail.com>
2025-11-13 15:46:33 +0000

    New image and edits to chicken soup

--- src/data/meals/saoto-poule-au-pot.ts
+++ src/data/meals/saoto-poule-au-pot.ts
@@ -3,28 +3,28 @@ import { IngredientId, SeasoningStapleId } from "@/models/ingredients";
 import { CookingMethodTag, BaseTag, ProteinSourceTag, ConvenienceTag } from "@/models/tagDefinitions";
 
 export const saotoPouleAuPot: Meal = {
-  id: "saoto-poule-au-pot",
-  name: "Saoto-style Poule-au-pot",
-  imageUrl: "./img-meals/poule-au-pot.jpg",
+  id: "saoto-chicken-soup",
+  name: "Saoto-style chicken soup",
+  imageUrl: "./img-meals/saoto-chicken-soup.jpg",
   tools: ["Pan", "Spatula"],
   steps: {
     prep: [
       "Cut the carrots and parsnips into bite-sized pieces",
-      "Cut the the onions and leeks into large slices",
+      "Cut the cabbage into small pieces (if needed)",
     ],
     cook: [
       "Start slow cooker on high, add water with chicken broth",
       "Sear chicken in pan until golden on the outside",
       "Once the water is hot enough, add chicken to slow cooker",
-      "Add carrots, parsnips, cloves, bouquet garni and a sprinkle of tarragon to slow cooker",
-      "Add some butter and white wine to slow cooker",
+      "Add carrots, parsnips, cabbage, cloves, bouquet garni and a sprinkle of tarragon to slow cooker",
+      "Add some white wine to slow cooker",
       "Leave to cook for 30 min",
       "Cook rice in pan with chicken broth from slow-cooker",
       "At the end of cooking, mix in bean sprouts",
     ],
   },
-  ingredients: [IngredientId.CHICKEN, IngredientId.CARROTS, IngredientId.PARSNIPS, IngredientId.WHITE_WINE, IngredientId.CHICKEN_BROTH, IngredientId.BEAN_SPROUTS],
-  seasoning: [SeasoningStapleId.BUTTER, SeasoningStapleId.BOUQUET_GARNI, SeasoningStapleId.BLACK_PEPPER, SeasoningStapleId.CLOVES, SeasoningStapleId.TARRAGON],
+  ingredients: [IngredientId.CHICKEN, IngredientId.CARROTS, IngredientId.PARSNIPS, IngredientId.CABBAGE, IngredientId.WHITE_WINE, IngredientId.CHICKEN_BROTH, IngredientId.BEAN_SPROUTS],
+  seasoning: [SeasoningStapleId.BOUQUET_GARNI, SeasoningStapleId.BLACK_PEPPER, SeasoningStapleId.CLOVES, SeasoningStapleId.TARRAGON],
   tags: {
     cookingMethod: CookingMethodTag.SLOW_COOKER,
     base: BaseTag.NONE,
